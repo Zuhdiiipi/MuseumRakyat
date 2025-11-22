7520c6fd2a64d7e0dbc4887073486b454060f39c
ZPuh <muhammadzuhdi391@gmail.com>
2025-11-22 15:31:18 +0800

    memperbaiki migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable()->unique();
             $table->string('password');
+            $table->enum('role', ['USER', 'CURATOR', 'ADMIN'])->default('USER');
             $table->rememberToken();
             $table->timestamps();
         });
