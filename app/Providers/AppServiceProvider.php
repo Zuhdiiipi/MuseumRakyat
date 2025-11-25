b96c6d4fea29b34cd95401a876d19467f10f2b3d
ZPuh <muhammadzuhdi391@gmail.com>
2025-11-25 15:43:29 +0800

    ngrok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- TAMBAHKAN BARIS INI
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // TAMBAHKAN KODE INI:
+        // Jika APP_URL di .env dimulai dengan 'https', paksa semua link jadi https
+        if (str_contains(config('app.url'), 'https')) {
+            URL::forceScheme('https');
+        }
     }
 }
